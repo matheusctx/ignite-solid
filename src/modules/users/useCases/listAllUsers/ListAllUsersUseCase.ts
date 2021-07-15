de817cd6de190656bce5298c33a5002676e25679
Matheus Teixeira <matheusctx@pop-os.localdomain>
2021-07-15 10:26:40 -0300

    Challenge finished

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,15 +1,27 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    if (user.admin === false) {
+      throw new Error('User does not admin');
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
